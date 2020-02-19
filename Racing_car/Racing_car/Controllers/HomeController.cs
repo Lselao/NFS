f363417642035b4a40a25392d1d2abfe24800156
Lebogang Selao <lebose@bbd.co.za>
2020-02-19 16:50:54 +0200

    NFS

---
+++ Racing_car/Racing_car/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Racing_car.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Racing_car.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly Db_context _Context;
+
+        public HomeController(Db_context context)
+        {
+            _Context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //var sessions = new Sessions() { UserName = "", highScore = 0 };
+            return View();
+        }
+
+        public IActionResult Leaderboard()
+        {
+            var leaderboard = _Context.Users.ToList();
+            return View(leaderboard);
+        }
+
+        //game page method
+        public IActionResult Game()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Game([Bind("Name")]Users user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _Context.Users.Add(user);
+                _Context.SaveChanges();
+                return RedirectToAction("Game");
+            }
+            return View("index", user);
+            //return View("Game", user);
+        }
+
+
+    }
+}
